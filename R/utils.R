f89aeac615438d0f00d7213469c0b5d3b38ddfda
Karl_Hailperin <khailper@gmail.com>
2018-08-22 17:08:05 -0500

    Let's make sure we're respecting API limits.

--- R/utils.R
+++ R/utils.R
@@ -15,6 +15,21 @@ base_url <- "https://api.scryfall.com/"
 set_url <- paste0(base_url, "sets")
 
 
+# function for enforcing rate limits
+# currently none exist, but Scryfall requests 50-100 ms delay
+# (https://scryfall.com/docs/api)
+polite_rate_limit <- function(delay){
+  if(delay < 50){
+    stop()
+  }
+  Sys.sleep(delay/1000)
+}
+
 # functions for checking function arguements
 # 
 # testing if set code is accepted 3 character code
+
+
+# set user agent per httr vignette
+# (https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html)
+ua <- user_agent("http://github.com/khailper/scryr")
