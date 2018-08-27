e2ee063dbe8bdbd0d5ae504ae3fbc314f5477271
Karl_Hailperin <khailper@gmail.com>
2018-08-26 19:56:09 -0500

    Reworte tests to actually test what they're supposed to.

--- tests/testthat/test-scry_sets.R
+++ tests/testthat/test-scry_sets.R
@@ -2,14 +2,16 @@ context("test-scry_sets")
 library(scryr)
 
 test_that("sets have all the columns", {
-  expect_equal(names(scry_sets("m19"), 
-                                c("code", "mtgo_code", "name", "set_type",
-                                  "release_at", "block_code", "block",
-                                  "parent_set_code", "card_count",
-                                  "digital", "foil_only", "icon_svg_uri")))
+  expect_equal(names(scry_sets("aer")), 
+                                c("object","code", "mtgo_code", "name", "uri",
+                                  "scryfall_uri", "search_uri", "released_at", 
+                                  "set_type", "card_count", "digital", 
+                                  "foil_only", "block_code", 
+                                  "block", "icon_svg_uri"))
 })
 
-test_that("only one set returned", {
+test_that("only one set returned and it's the right type", {
+  expect_true(tibble::is_tibble(scry_sets("m19")))
   expect_equal(nrow(scry_sets("m19")), 1)
 })
 
@@ -19,4 +21,8 @@ test_that("set returns error if wrong code used", {
   expect_error(scry_sets("m190"), "Error message for non-existant set")
 })
 
+test_that("rate limit works"{
+  expect_error(scry_sets("m19", delay = 25), 
+               "Please respect Scryfall's rate limit.")
+})
 
