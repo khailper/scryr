3b23a2dbec0a5ce9dacc5abfdd2e38d4905ef70d
Karl_Hailperin <khailper@gmail.com>
2018-08-30 19:29:17 -0500

    First pass at tests.

---
+++ tests/testthat/test-scry_catalog.R
@@ -0,0 +1,10 @@
+context("test-scry_catalog")
+
+test_that("scry_catalog returns a character vector", {
+  expect_equal(typeof(scry_catalog("artist-names"), "character"))
+})
+
+test_that("scry_catalog returns informative error if catalog doesn't exist", {
+  expect_equal(typeof(scry_catalog("crd-names"), 
+                      "That catalog doesn't exist. ?scry_catalog for list of available catalogs."))
+})
