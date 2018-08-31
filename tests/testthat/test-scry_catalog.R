d37c113687e3dfda06691c585f828567697563f6
Karl_Hailperin <khailper@gmail.com>
2018-08-30 20:15:19 -0500

    Fixed error-inducing typo.

--- tests/testthat/test-scry_catalog.R
+++ tests/testthat/test-scry_catalog.R
@@ -1,7 +1,7 @@
 context("test-scry_catalog")
 
 test_that("scry_catalog returns a character vector", {
-  expect_equal(typeof(scry_catalog("artist-names"), "character"))
+  expect_equal(typeof(scry_catalog("artist-names")), "character")
 })
 
 test_that("scry_catalog returns informative error if catalog doesn't exist", {
