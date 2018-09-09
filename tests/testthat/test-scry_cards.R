c723389eb76911a17b7987a788653b58d564becb
Karl_Hailperin <khailper@gmail.com>
2018-09-09 15:33:39 -0500

    Added ideas for what I should test.

---
+++ tests/testthat/test-scry_cards.R
@@ -0,0 +1,17 @@
+context("test-scry_cards")
+
+test_that("scry_cards parse search query correctly", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("scry_cards parse handles bad searches", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("pagination works for two pages", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("pagination works for three pages", {
+  expect_equal(2 * 2, 4)
+})
