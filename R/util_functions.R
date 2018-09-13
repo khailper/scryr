7e23e4fb98a2fab0afde7e0e9a879a6d3cb2d7d9
Karl_Hailperin <khailper@gmail.com>
2018-09-13 14:16:09 -0500

    Splitting into two files to avoid bloated files. Also, moving towards complience with tidyverse style guidelines.

---
+++ R/util_functions.R
@@ -0,0 +1,59 @@
+#' @importFrom attempt stop_if_not
+#' @importFrom curl has_internet
+#' @importFrom httr user_agent
+#' @importFrom dplyr bind_rows
+#' 
+check_internet <- function(){
+  stop_if_not(.x = has_internet(), 
+              msg = "Please check your internet connection")
+}
+
+#' @importFrom httr status_code
+check_status <- function(res){
+  # long-term goal: customize to Scryfall's error messages
+  stop_if_not(.x = status_code(res), 
+              .p = ~ .x == 200,
+              msg = "The API returned an error")
+}
+#' 
+#' Polite rate limit
+#' 
+#' A function for enforcing rate limits.
+#' 
+#' @description 
+#' Adds a delay before executing scry_* functions. Currently no formal rate 
+#' limit exists on the Scryfall api, but they request 50-100 ms delays between 
+#' queries.
+#' 
+#' @param delay
+#' Number of microseconds scryr should wait between requests (Scryfall asks for 
+#' 50-100).
+#' (https://scryfall.com/docs/api)
+polite_rate_limit <- function(delay){
+  if(delay < 50){
+    stop("Please respect Scryfall's rate limit.")
+  }
+  Sys.sleep(delay/1000)
+}
+#' functions for checking function arguments
+#' 
+#' 
+#' function for handling large responses
+#'  
+#' @param current_data Data from last page pulled.
+#' @param next_page_uri 
+#' URI for next page of data. This will be taken from the JSON response of the 
+#' current page
+#' @param delay
+#' Number of microseconds scryr should wait between requests (Scryfall asks for 
+#' 50-100). There's no default value because it will be carried over from 
+#' calling function.
+#' 
+#' @noRd
+handle_pagination <- function(current_data, next_page_uri, delay){
+  # plan: pull data from next_page_uri, check data for another next_page,
+  # if there, call handle_pagination; finally bind results to current_data
+  bind_rows(current_data, next_page_data)
+}
+
+
---
+++ R/util_objects.R
@@ -0,0 +1,16 @@
+#' set user agent per httr vignette
+#' (https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html)
+ua <- user_agent("http://github.com/khailper/scryr")
+#'
+#' set up urls functions will need for API queries
+base_url <- "https://api.scryfall.com/"
+set_url <- paste0(base_url, "sets/")
+catalog_url <- paste0(base_url, "catalog/")
+card_search_url <- paste0(base_url, "sets/cards/search")
+#'
+#'list of available catalogs so scry_catalog() can throw error
+#' source: https://scryfall.com/docs/api/catalogs
+catalog_list <- c("card-names", "artist-names", "word-bank", "creature-types",
+                  "planeswalker-types", "land-types","artifact-types",
+                  "enchantment-types", "spell-types", "powers", "toughnesses",
+                  "loyalties", "watermarks")
