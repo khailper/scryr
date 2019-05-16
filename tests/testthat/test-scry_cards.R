48bc9718589f17dee62f33eb0f5c402b352f7f0b
Karl_Hailperin <khailperin@data-blitz.com>
2019-05-16 08:51:49 -0500

    basic ways function can fail

--- tests/testthat/test-scry_cards.R
+++ tests/testthat/test-scry_cards.R
@@ -7,11 +7,16 @@ test_that("scry_cards parse search query correctly", {
 test_that("scry_cards parse handles bad searches", {
   expect_equal(2 * 2, 4)
 })
-
-test_that("pagination works for two pages", {
-  expect_equal(2 * 2, 4)
+test_that("scry_cards parse handles bad searches", {
+  expect_error(scry_cards(query = "set:ktk", .unique = "card"), 
+               ".unique must be one of 'cards', 'part', or 'prints'")
 })
 
-test_that("pagination works for three pages", {
-  expect_equal(2 * 2, 4)
+test_that("pagination works for two pages", {
+  expect_equal(nrow(scry_cards(query = "set:ktk")), 269)
+})
+
+test_that("pagination works for three or more pages", {
+  # 688 from manual search
+  expect_equal(nrow(scry_cards(query = "b:ktk")), 688)
 })
