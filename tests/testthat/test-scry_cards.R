4d6b5c34185cb96e8ac15c73aa1194ed5bdc4768
Karl_Hailperin <khailperin@data-blitz.com>
2019-05-16 10:11:25 -0500

    duplicate test name

--- tests/testthat/test-scry_cards.R
+++ tests/testthat/test-scry_cards.R
@@ -4,9 +4,7 @@ test_that("scry_cards parse search query correctly", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("scry_cards parse handles bad searches", {
-  expect_equal(2 * 2, 4)
-})
+
 test_that("scry_cards parse handles bad searches", {
   expect_error(scry_cards(query = "set:ktk", .unique = "card"), 
                ".unique must be one of 'cards', 'part', or 'prints'")
