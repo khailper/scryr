7b602addf3a11566f7221cfea203e48e25278c27
Karl_Hailperin <khailperin@data-blitz.com>
2019-05-16 10:42:34 -0500

    using internal data; keeping file for reproducibility

--- R/util_objects.R
+++ R/util_objects.R
@@ -2,19 +2,19 @@
 #' @importFrom httr user_agent
 #' @noRd
 # based on: https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html
-scryr_ua <- user_agent("http://github.com/khailper/scryr")
+# scryr_ua <- httr::user_agent("http://github.com/khailper/scryr")
 #'
 #' set up urls functions will need for API queries
 #' @noRd
-scryr_base_url <- "https://api.scryfall.com/"
-scryr_set_url <- paste0(scryr_base_url, "sets/")
-scryr_catalog_url <- paste0(scryr_base_url, "catalog/")
-scryr_card_search_url <- paste0(scryr_base_url, "sets/cards/search?q=")
+# scryr_base_url <- "https://api.scryfall.com/"
+# scryr_set_url <- paste0(scryr_base_url, "sets/")
+# scryr_catalog_url <- paste0(scryr_base_url, "catalog/")
+# scryr_card_search_url <- paste0(scryr_base_url, "sets/cards/search?q=")
 #'
 #'list of available catalogs so scry_catalog() can throw error
 #' source: https://scryfall.com/docs/api/catalogs
 #' @noRd
-catalog_list <- c("card-names", "artist-names", "word-bank", "creature-types",
-                  "planeswalker-types", "land-types","artifact-types",
-                  "enchantment-types", "spell-types", "powers", "toughnesses",
-                  "loyalties", "watermarks")
+# catalog_list <- c("card-names", "artist-names", "word-bank", "creature-types",
+#                   "planeswalker-types", "land-types","artifact-types",
+#                   "enchantment-types", "spell-types", "powers", "toughnesses",
+#                   "loyalties", "watermarks")
