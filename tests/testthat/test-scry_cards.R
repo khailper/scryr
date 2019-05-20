f2621a20cd81eb8bef6805531558ad5c7d58d056
Karl_Hailperin <khailperin@data-blitz.com>
2019-05-20 15:18:22 -0500

    covering both good and bad searches

--- tests/testthat/test-scry_cards.R
+++ tests/testthat/test-scry_cards.R
@@ -1,11 +1,12 @@
 context("test-scry_cards")
 
-test_that("scry_cards parse search query correctly", {
-  expect_equal(2 * 2, 4)
+test_that("scry_cards parses search query correctly", {
+  expect_equal(nrow(scry_cards("set:rtr has:watermark")), 138)
+  expect_error(scry_cards("set:ktk wm:mardu c:u"))
 })
 
 
-test_that("scry_cards parse handles bad searches", {
+test_that("scry_cards handles bad searches", {
   expect_error(scry_cards(query = "set:ktk", .unique = "card"), 
                ".unique must be one of 'cards', 'art', or 'prints'")
 })
