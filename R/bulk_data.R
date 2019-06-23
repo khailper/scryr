1b9f28c161d894b66807e68b3d95c08240bc731a
Karl_Hailperin <khailperin@data-blitz.com>
2019-06-23 08:46:29 -0500

    First pass

--- R/bulk_data.R
+++ R/bulk_data.R
@@ -28,10 +28,13 @@ bulk_rulings <- function(){
   tibble::as_tibble(jsonlite::fromJSON(rawToChar(res$content)))
 }
 
+#' @param include_ids Should results include ID variables (e.g. Oracle ID)
+#' @param include_uris Should results include URI variables (e.g. the URL for
+#' the card on Scryfall)
 #' @concept bulk_data
 #' @rdname bulk_data
 #' @export
-bulk_default_cards <- function(){
+bulk_default_cards <- function(include_ids = FALSE, include_uris = FALSE){
   query_url <- paste0(bulk_data_url, "default-cards.json")
   
   # Check for internet
@@ -43,5 +46,83 @@ bulk_default_cards <- function(){
   # Check the result
   check_status(res)
   
-  tibble::as_tibble(jsonlite::fromJSON(rawToChar(res$content)))
+  bulk_data <- tibble::as_tibble(jsonlite::fromJSON(rawToChar(res$content)))
+  
+  if (!include_ids){
+    bulk_data <- dplyr::select(bulk_data, 
+                               -dplyr::ends_with("id"), 
+                               -dplyr::ends_with("ids"))
+  }
+  
+  if (!include_uris){
+    bulk_data <- dplyr::select(bulk_data, 
+                               -dplyr::contains("uri"),
+                               -dplyr::contains("uris"))
+  }
+  
+  return(bulk_data)
+}
+
+#' @concept bulk_data
+#' @rdname bulk_data
+#' @export
+bulk_oracle_cards <- function(include_ids = FALSE, include_uris = FALSE){
+  query_url <- paste0(bulk_data_url, "oracle-cards.json")
+  
+  # Check for internet
+  check_internet()
+  
+  # Get search results
+  res <- httr::GET(query_url, scryr_ua)
+  
+  # Check the result
+  check_status(res)
+  
+  bulk_data <- tibble::as_tibble(jsonlite::fromJSON(rawToChar(res$content)))
+  
+  if (!include_ids){
+    bulk_data <- dplyr::select(bulk_data, 
+                               -dplyr::ends_with("id"), 
+                               -dplyr::ends_with("ids"))
+  }
+  
+  if (!include_uris){
+    bulk_data <- dplyr::select(bulk_data, 
+                               -dplyr::contains("uri"),
+                               -dplyr::contains("uris"))
+  }
+  
+  return(bulk_data)
+}
+
+#' @concept bulk_data
+#' @rdname bulk_data
+#' @export
+bulk_all_cards <- function(include_ids = FALSE, include_uris = FALSE){
+  query_url <- paste0(bulk_data_url, "all-cards.json")
+  
+  # Check for internet
+  check_internet()
+  
+  # Get search results
+  res <- httr::GET(query_url, scryr_ua)
+  
+  # Check the result
+  check_status(res)
+  
+  bulk_data <- tibble::as_tibble(jsonlite::fromJSON(rawToChar(res$content)))
+  
+  if (!include_ids){
+    bulk_data <- dplyr::select(bulk_data, 
+                               -dplyr::ends_with("id"), 
+                               -dplyr::ends_with("ids"))
+  }
+  
+  if (!include_uris){
+    bulk_data <- dplyr::select(bulk_data, 
+                               -dplyr::contains("uri"),
+                               -dplyr::contains("uris"))
+  }
+  
+  return(bulk_data)
 }
