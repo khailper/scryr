4e5c9a594b7e82a4a61f605dbcf803b59f870361
Karl_Hailperin <khailperin@data-blitz.com>
2019-06-23 20:25:11 -0500

    carrying lessons from label_guild to label_tri

--- tests/testthat/test-label-functions.R
+++ tests/testthat/test-label-functions.R
@@ -21,67 +21,67 @@ test_that("label_guild(inclusive = TRUE) corectly labels a 1 color card", {
 })
 
 test_that("label_tri doesn't assign guilds to cards without exactly 3 colors", {
-  expect_true(is.na(label_tri(list("B"))))
-  expect_true(is.na(label_tri(list("B", "G"))))
-  expect_true(is.na(label_tri(list("B", "G", "R", "U"))))
-  expect_true(is.na(label_tri(list("B", "G", "R", "U", "W"))))
+  expect_true(is.na(label_tri("B")))
+  expect_true(is.na(label_tri(("B", "G"))))
+  expect_true(is.na(label_tri(c("B", "G", "R", "U"))))
+  expect_true(is.na(label_tri(c("B", "G", "R", "U", "W"))))
 })
 
 test_that("label_tri(inclusive = TRUE) doesn't assign guilds to cards with >3 colors", {
-  expect_true(is.na(label_tri(list("B", "G", "R", "U"), inclusive = TRUE)))
-  expect_true(is.na(label_tri(list("B", "G", "R", "U", "W"), inclusive = TRUE)))
+  expect_true(is.na(label_tri(c("B", "G", "R", "U"), inclusive = TRUE)))
+  expect_true(is.na(label_tri(c("B", "G", "R", "U", "W"), inclusive = TRUE)))
 })
 
 test_that("label_tri corectly labels a 3 color card", {
-  expect_equal(label_tri(list("B", "G", "U")), list("Sultai"))
-  expect_equal(label_tri(list("B", "G", "U"), shard_or_wedge = "either"), 
+  expect_equal(label_tri(c("B", "G", "U")), list("Sultai"))
+  expect_equal(label_tri(c("B", "G", "U"), shard_or_wedge = "either"), 
                list("Sultai"))  
-  expect_equal(label_tri(list("B", "G", "U"), shard_or_wedge = "wedge"), 
+  expect_equal(label_tri(c("B", "G", "U"), shard_or_wedge = "wedge"), 
                list("Sultai"))
-  expect_true(is.na(label_tri(list("B", "G", "U"), shard_or_wedge = "shard")))
-  expect_equal(label_tri(list("B", "G", "R")), list("Jund"))
-  expect_equal(label_tri(list("B", "G", "R"), shard_or_wedge = "either"), 
+  expect_true(is.na(label_tri(c("B", "G", "U"), shard_or_wedge = "shard")))
+  expect_equal(label_tri(c("B", "G", "R")), list("Jund"))
+  expect_equal(label_tri(c("B", "G", "R"), shard_or_wedge = "either"), 
                list("Jund"))  
-  expect_equal(label_tri(list("B", "G", "R"), shard_or_wedge = "shard"), 
+  expect_equal(label_tri(c("B", "G", "R"), shard_or_wedge = "shard"), 
                list("Jund"))
-  expect_true(is.na(label_tri(list("B", "G", "R"), shard_or_wedge = "wedge")))
+  expect_true(is.na(label_tri(x("B", "G", "R"), shard_or_wedge = "wedge")))
 })
 
 test_that("label_tri(inclusive = TRUE) corectly labels a 1 or 2 color card", {
-  expect_equal(label_tri(list("B"), 
+  expect_equal(label_tri("B", 
                          inclusive = TRUE), 
-               list("Esper", "Grixis", "Jund", "Abzan", "Mardu", "Sultai"))
-  expect_equal(label_tri(list("B"), 
+               list(c("Esper", "Grixis", "Jund", "Abzan", "Mardu", "Sultai")))
+  expect_equal(label_tri("B", 
                          inclusive = TRUE, 
                          shard_or_wedge = "either"), 
-               list("Esper", "Grixis", "Jund", "Abzan", "Mardu", "Sultai"))
-  expect_equal(label_tri(list("B"), 
+               list(c("Esper", "Grixis", "Jund", "Abzan", "Mardu", "Sultai")))
+  expect_equal(label_tri("B", 
                          inclusive = TRUE, 
                          shard_or_wedge = "shard"), 
-               list("Esper", "Grixis", "Jund"))
-  expect_equal(label_tri(list("B"), 
+               list(c("Esper", "Grixis", "Jund")))
+  expect_equal(label_tri("B", 
                          inclusive = TRUE, 
                          shard_or_wedge = "wedge"), 
-               list("Abzan", "Mardu", "Sultai"))
-  expect_equal(label_tri(list("B", "G"), 
+               list(c("Abzan", "Mardu", "Sultai")))
+  expect_equal(label_tri(c("B", "G"), 
                          inclusive = TRUE), 
-               list("Jund", "Abzan", "Sultai"))
-  expect_equal(label_tri(list("B", "G"), 
+               list(c("Jund", "Abzan", "Sultai")))
+  expect_equal(label_tri(c("B", "G"), 
                          inclusive = TRUE, 
                          shard_or_wedge = "either"), 
-               list("Jund", "Abzan", "Sultai"))
-  expect_equal(label_tri(list("B", "G"), 
+               list(c("Jund", "Abzan", "Sultai")))
+  expect_equal(label_tri(c("B", "G"), 
                          inclusive = TRUE, 
                          shard_or_wedge = "shard"), 
                list("Jund"))
-  expect_equal(label_tri(list("B", "G"), 
+  expect_equal(label_tri(c("B", "G"), 
                          inclusive = TRUE, 
                          shard_or_wedge = "wedge"), 
-               list("Abzan", "Sultai"))
+               list(c("Abzan", "Sultai")))
 
 })
 
 
 test_that("label_tri checks shard_or_wedge parameter correctly", {
-  expect_error(abel_tri(list("B", "G", "U"), shard_or_wedge = "wdge"))
+  expect_error(label_tri(lc("B", "G", "U"), shard_or_wedge = "wdge"))
 })
