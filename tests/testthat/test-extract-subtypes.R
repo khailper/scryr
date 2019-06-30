5edf632d3ee8bfd64f375eece8f139e831a01096
Karl_Hailperin <khailperin@data-blitz.com>
2019-06-29 20:05:17 -0500

    str_split returns a list

--- tests/testthat/test-extract-subtypes.R
+++ tests/testthat/test-extract-subtypes.R
@@ -1,17 +1,22 @@
 test_that("extract_subtypes() works", {
-  expect_equal(extract_subtypes("Creature - Merfolk Wizard"), "Merfolk Wizard")
-  expect_equal(extract_subtypes("Artifact Creature - Golem"), "Golem")
-  expect_equal(extract_subtypes("Instant - Arcane"), "Arcane")
+  # dash (emdash or endash) comes through wrong
+  expect_equal(extract_subtypes("Creature â€” Merfolk Wizard"), 
+               list("Merfolk Wizard"))
+  expect_equal(extract_subtypes("Artifact Creature â€” Golem"), 
+               list("Golem"))
+  expect_equal(extract_subtypes("Instant â€” Arcane"), 
+               list("Arcane"))
 })
 
 test_that("extract_subtypes(split = TRUE) works", {
-  expect_equal(extract_subtypes("Creature - Merfolk Wizard", split = TRUE),
-               c("Merfolk", "Wizard"))
-  expect_equal(extract_subtypes("Instant - Arcane", split = TRUE),
-               c("Arcane"))
+  expect_equal(extract_subtypes("Creature â€” Merfolk Wizard", split = TRUE),
+               list(c("Merfolk", "Wizard")))
+  expect_equal(extract_subtypes("Instant â€” Arcane", split = TRUE),
+               list(c("Arcane")))
 })
 
 test_that("extract_subtypes fails for non-strings and lack of subtype",{
   expect_error(extract_subtypes(5))
   expect_true(is.na(extract_subtypes("Instant")))
+  expect_true(is.na(extract_subtypes("Instant", split = TRUE)))
 })
