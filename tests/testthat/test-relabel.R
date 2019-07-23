a840e5ec7478f32661f7952fd1d56ba7c32f5715
Karl_Hailperin <khailperin@data-blitz.com>
2019-07-23 10:25:50 -0500

    found test coverage gaps

--- tests/testthat/test-relabel.R
+++ tests/testthat/test-relabel.R
@@ -2,4 +2,8 @@ test_that("relabel_mtg_color() works", {
   expect_equal(relabel_mtg_color("G"), "Green")
   expect_equal(relabel_mtg_color(c("G", "U")), "Multicolored")
   expect_equal(relabel_mtg_color(c()), "Colorless")
+  expect_equal(relabel_mtg_color("Green"), NA_character_)
+})
+test_that("relabel_mtg_color() returns an error when it should", {
+  expect_error(relabel_mtg_color(2), "color_code needs to be a character vector")
 })
